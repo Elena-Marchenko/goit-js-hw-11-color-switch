e2553f9dff0a27b5a59e8e42f78a9fd19b6174ff
Elena-Marchenko <Leawest91@gmail.com>
2021-05-11 18:43:47 +0300

    Готовая версия дз

---
+++ index.js
@@ -0,0 +1,45 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const COLOR_CHANGE_TIME = 1000;
+
+const refs = {
+    body: document.querySelector('body'),
+    buttonStart: document.querySelector('[data-action="start"]'),
+    buttonStop: document.querySelector('[data-action="stop"]'),
+};
+
+refs.buttonStart.addEventListener('click', onButtonStart);
+refs.buttonStop.addEventListener('click', onButtonStop);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function changeBackgroundColor(color) {
+    refs.body.style.backgroundColor = color;
+}
+
+let timerColor = null;
+
+function onButtonStart() {
+    timerColor = setInterval(randomNumber => {
+        randomNumber = randomIntegerFromInterval(0, 5);
+        changeBackgroundColor(colors[randomNumber]);
+    }, COLOR_CHANGE_TIME);
+    refs.buttonStart.setAttribute('disabled' ,true);
+    
+}
+
+function onButtonStop() {
+    clearInterval(timerColor);
+    refs.buttonStart.removeAttribute('disabled');
+    
+}
+
